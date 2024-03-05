f68988f276de574b164c90b86709bff1f5a3d0d9
Armando <arodrigues2010@hotmail.com>
2024-03-05 15:44:21 +0100

    fix

--- src/components/CustomersForm.js
+++ src/components/CustomersForm.js
@@ -3,16 +3,17 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { TextField, Button } from '@mui/material';
 
+
 const UserForm = () => {
   const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
   const axiosInstance = useMemo(() => axios.create({
-    baseURL: 'https://localhost:44335/api/v1/Sales',
+    baseURL: 'https://localhost:44335/api/v1/Sales', // Corregido el método de creación de axiosInstance
     timeout: 10000,
     headers: {
       'Content-Type': 'application/json',
     },
-  }), ['https://localhost:44335/api/v1/Sales']);
+  }), []); // Eliminado el array de dependencias
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -24,28 +25,50 @@ const UserForm = () => {
 
   const onSubmit = async (data) => {
     try {
-      await axiosInstance.post('/Create', data);
+      await axiosInstance.post('/Create', data); 
       reset();
     } catch (error) {
       console.error('Error creating user:', error);
     }
   };
 
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
         label="First Name"
         variant="outlined"
-        {...register("firstName", { required: true })}
+        {...register("first", { required: true })}
         error={!!errors.firstName}
         helperText={errors.firstName && "First name is required"}
       />
       <TextField
         label="Last Name"
         variant="outlined"
-        {...register("lastName", { required: true })}
+        {...register("last", { required: true })}
         error={!!errors.lastName}
         helperText={errors.lastName && "Last name is required"}
+      />
+      <TextField
+        label="Company"
+        variant="outlined"
+        {...register("company", { required: true })}
+        error={!!errors.lastName}
+        helperText={errors.lastName && "Company is required"}
+      />
+      <TextField
+        label="Country"
+        variant="outlined"
+        {...register("country", { required: true })}
+        error={!!errors.lastName}
+        helperText={errors.lastName && "Country is required"}
+      />
+      <TextField
+        label="Email"
+        variant="outlined"
+        {...register("email", { required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ })}
+        error={!!errors.email}
+        helperText={errors.email && "Please enter a valid email"}
       />
       <Button type="submit" variant="contained" color="primary" size="large">
         Submit
